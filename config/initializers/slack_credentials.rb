42b686ac63642a92ad15bb7fbbeaf49441289a6f
Paul N. Ingelmo <paulingelmo@gmail.com>
2016-02-11 12:44:45 -0500

    hiding config variables

--- config/initializers/slack_credentials.rb
+++ config/initializers/slack_credentials.rb
@@ -1,7 +1,7 @@
 require 'slack'
 
 Slack.configure do |config|
-  config.token = 'xoxp-5186493494-17723172612-20660303509-a908b26010'
+  config.token = ENV['SLACK_TOKEN']
 end
 
-$slack_client = Slack::Client.new token: 'xoxp-5186493494-17723172612-20660303509-a908b26010'
+$slack_client = Slack::Client.new token: ENV['SLACK_TOKEN']
--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 
 $twitter_client = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'rzjEdqDc660dDQhFIgiHTRHNY'
-  config.consumer_secret = 'EQ6A6GPeH4rS7Mo5fCzrlDzMLIy3fkJ0TJINc6im37w54nFZmJ'
-  config.access_token = '1416537956-wr6qsreZUzCH2Zo867rTkemDw3yG1uF6a7d6hfY'
-  config.access_token_secret = 'oD0PFDDPJM0JY7SvlWpOa98IF8UpCdafDuhympJ5AYtjY'
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
